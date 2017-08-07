dbfe4bf6549914c2a8873c7003ad002bd1824936
james.alcaraz <alcarazjames@gmail.com>
2017-08-07 17:46:36 +0800

    Setup bet type selector

--- BettingCalculator.Web/app/js/betCalculator.js
+++ BettingCalculator.Web/app/js/betCalculator.js
@@ -2,11 +2,22 @@
 (function () {
 
     $(document).ready(function () {
-        
+        chosenHandler('#betTypeId');
         //$('#calculateButton').click(clickHandler);
     });
 
-    function clickHandler() {
-        alert('whoa');
+    function chosenHandler(selector) {
+        //$(selector).chosen({
+        //    disable_search_threshold: 10,
+        //    no_results_text: "Oops, nothing found!"
+            
+        //});
+        $(selector).selectpicker({
+            size: 'auto',
+            liveSearch: true,
+            width: '100%',
+            noneSelectedText: 'Please select bet type'
+        });
+
     }
 })();
